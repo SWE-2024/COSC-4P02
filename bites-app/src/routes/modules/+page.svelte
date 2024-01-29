d87577476883c7bf4fc5b435334bbaf10962b434
Vinit Udasi <vinitudasi8888@gmail.com>
2024-01-29 05:48:40 -0500

    set up basic modules page

---
+++ bites-app/src/routes/modules/+page.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Content from './Content.svelte'
+	import Sidebar from './Sidebar.svelte'
+
+	let selectedIndex = 1;
+
+
+</script>
+
+<svelte:head>
+	<title>Modules</title>
+	<meta name="description" content="Read the modules" />
+</svelte:head>
+
+<section>
+		<Sidebar bind:selectedIndex/>
+		<Content bind:selectedIndex/>
+	
+</section>
+
+
+<style>
+
+	section{
+		margin-top: 1rem;
+		width: 95vw;
+		height: 90vh;
+		overflow-y: hidden;
+		margin-inline: auto;
+		display: flex;
+
+	}
+</style>
