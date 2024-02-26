4ed27758d3b79935ba6dd6e20cfd1417d4f83630
julellisg <julianellisg@gmail.com>
2024-02-26 16:45:08 -0500

    Added both footers with working links
    Includes "Site Links" and "Related Links"
    
    May require a contact section or column for other information, as well as adding a "BITES" Logo

--- bites-app/src/components/Footer.svelte
+++ bites-app/src/components/Footer.svelte
@@ -1,23 +1,61 @@
 <script>
 </script>
 
+<!-- <li><a href="./home">Home</a></li>
+		<li><a href="./modules">Modules</a></li>
+		<li><a href="./quizzes">Quizzes</a></li>
+		<li><a href="./libraries">Libraries</a></li> -->
+
 <main>
 	<footer>
-		<div>
-			<p>© Brock Interactive Training for Engineering Students 2024. All Rights Reserved.</p>
+		<div class="footer-links">
+			<div class="column">
+				<p class="footer-title">Site Links</p>
+				<p><a href="./home">Home</a></p>
+				<p><a href="./modules">Modules</a></p>
+                <p><a href="./quizzes">Quizzes</a></p>
+                <p><a href="./libraries">Libraries</a></p>
+			</div>
+			<div class="column">
+				<p class="footer-title">Related Links</p>
+				<p><a href="https://github.com/SWE-2024/COSC-4P02">GitHub Repository</a></p>
+				<p><a href="https://brocku.ca/">Brock University</a></p>
+			</div>
+		</div>
+
+		<div class="second-div">
+			<p>© Niagara-on-the-Lake Museum 2024. All Rights Reserved.</p>
 		</div>
 	</footer>
 </main>
 
 <style>
 	footer {
-		background-color: rgb(170, 0 , 0);
+		background-color: rgba(0, 0, 0, 0.2);
+        border-top: 2px rgb(170, 0, 0) solid;
 		color: #fff;
-		padding: 12px;
-		text-align: center;
+		margin: 0;
+		/* text-align: center; */
 	}
 
-	.footer-content {
-		margin: 0 auto;
+	.footer-title {
+		font-weight: 500;
+		text-transform: none; /* Would make the text fully capitalized without this line */
+	}
+
+	.footer-links {
+		display: flex;
+		justify-content: center;
+		padding: 16px 12px;
+	}
+
+	.column {
+		padding: 0 64px; /* space between each column */
+	}
+
+	.second-div {
+        text-align: center;
+		background-color: rgb(170, 0, 0);
+		padding: 12px;
 	}
 </style>
