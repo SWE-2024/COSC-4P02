9523c3929d9d2488d6218dcd8ca960b81d06a55e
0-20 <49103023+0-20@users.noreply.github.com>
2024-02-28 13:29:55 -0500

    Part 2 of last commit

--- bites-app/src/routes/libraries/+page.svelte
+++ bites-app/src/routes/libraries/+page.svelte
@@ -1,105 +1,25 @@
+
 <svelte:head>
 	<title>Libraries</title>
 	<meta name="description" content="Index of Resources" />
 </svelte:head>
 
-<script>
+<script lang="ts">
+	import type { Load } from "@sveltejs/kit"
 	let columns = ["Name", "Description", "Site"]
-	let data = [
-    ["Wikipedia", 
-	"The free online encyclopedia", 
-	"https://en.wikipedia.org/wiki/Software_engineering"],
-
-    ["Library Genesis", 
-	"Library with many free books", 
-	"https://libgen.is/"],
-
-    ["Google", "The software engineer's best friend", 
-	"https://www.google.com/search?q=resources+for+learning+software+engineering"],
-
-	["Engineering Resources",
-	"Giant list of resources for several kinds of engineering students.",
-	"https://github.com/aaryan2134/Engineering-Resources"],
-
-	["Professional Programming",
-	"Another huge list of engineering resources.",
-	"https://github.com/charlax/professional-programming"],
-
-	["Awesome Agile", 
-	"List of resources related to the agile method.", 
-	"https://github.com/lorabv/awesome-agile"],
-
-	["Challenges of an Agile Workforce: Adopting an Agile Mindset",
-	"Article that explaines why the agile method even exists and what problems it tries to solve.",
-	"https://www.linkedin.com/pulse/challenges-agile-workforce-adopting-mindset-chris-gagn%25C3%25A9/"],
-
-	["Incremental Development", 
-	"Guide for learning incremental development in the context of the agile method.", 
-	"https://github.com/HackYourFutureBelgium/incremental-development"],
-
-	["Learn Scrum", 
-	"Starting point for learning scrum, a popular flavor of the agile method.", 
-	"https://github.com/imalitavakoli/learn-scrum"],
-
-	["Awesome Systems Engineering",
-	"Resources of examples of systems engineering.",
-	"https://github.com/kktse/awesome-systems-engineering"],
-
-	["Awesome Software Architecture", 
-	"Resources on software architecture and design patterns.", 
-	"https://github.com/mehdihadeli/awesome-software-architecture"],
-
-	["Testing strategies in a microservices architecture", 
-	"In depth guide on how to test a service.", 
-	"https://martinfowler.com/articles/microservice-testing/"],
-
-	["Intro To PyTest", 
-	"Tutorial on the Python testing framework PyTest.", 
-	"https://github.com/pluralsight/intro-to-pytest"],
-
-	["Web Automation Tutorial", 
-	"Tutorial on the basics of automating testing on websites with tools like Selenium.", 
-	"https://github.com/Anshul-Sonpure/WebAutomation_Tutorial"],
-
-	["Learn SQL", 
-	"Tutorial on learning SQL.", 
-	"https://github.com/WebDevSimplified/Learn-SQL"],
-
-	["Git Good: Common Commands", 
-	"Summary of the dozen git commands you will use 99% of the time.", 
-	"https://www.youtube.com/watch?v=b4zp02iULYY"],
-
-	["Docker - Beginners | Intermediate | Advanced", 
-	"Tutorials on learning Docker for running packages in environments so they don't conflict with the packages already on your OS.", 
-	"https://github.com/collabnix/dockerlabs"],
-
-	["Jenkins Tutorial", 
-	"Tutorial on Jenkins for automating the entire process of building, testing and deploying your software.", 
-	"https://github.com/ssbostan/jenkins-tutorial"],
-
-	["How to Win the Coding Interview", 
-	"An interview question example from the perspective of an interviewer.", 
-	"https://medium.com/on-writing-code/how-to-win-the-coding-interview-71ae7102d685"],
-
-	["Red Flags on Your Resume",
-	"Tips on how to write a better resume.",
-	"https://danicfilip.com/2020/red-flags-on-your-resume/"],
-
-	["Front End Interview Questions", 
-	"Quiz to test your knowledge on the front-end and agile method.", 
-	"https://github.com/rradfar/front-end-interview-questions"],
-  ]
+	export let data: Object
+	let library = data.props // fix this
 </script>
 
 <table class="library">
-	<tr>
+	<tr id="libraryTitles">
 		{#each columns as column}
 			<th>{column}</th>
 		{/each}
 	</tr>
 	
-	{#each data as row}
-		<tr>
+	{#each library.libraryArray as row}
+		<tr id="libraryEntries">
 			<!--
 			{#each row as cell}
 			<td contenteditable="false" bind:innerHTML={cell} />
@@ -107,7 +27,7 @@
 			-->
 			<td contenteditable="false" bind:innerHTML={row[0]} />
 			<td contenteditable="false" bind:innerHTML={row[1]} />
-			<a href={row[2]} target=”_blank” rel="noopener noreferrer" contenteditable="false" bind:innerHTML={row[2]} />
+			<td><a href={row[2]} target=”_blank” rel="noopener noreferrer" contenteditable="false" bind:innerHTML={row[2]} /></td>
 		</tr>
 	{/each}
 </table>
@@ -116,12 +36,25 @@
 	.library {
 		margin: 1% 0 2% 5%;
 		width: 90%;
-		background-color: aliceblue;
-		color: black;
+		background-color: #10101033;
+		color: #e0e8ef;
 		padding: 0 0 0 0;
-		border-radius: 5pt;
+		border-radius: 2pt;
 	}
+
 	th, td {
 		padding: 5px;
+		border-width: 1pt;
+		border-color: #00000033;
+		border-style: solid;
+	}
+
+	#libraryTitles {
+		background-color: #e0e8ef;
+		color: #000;
+	}
+
+	#libraryEntries:nth-child(even) {
+    	background-color: #00000033;
 	}
 </style>
