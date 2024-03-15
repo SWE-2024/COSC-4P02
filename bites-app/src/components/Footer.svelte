13a5a7f328600e9abcfbd7ef491e64ade46aa923
Shubham14-02 <shubhamamrelia1402@gmail.com>
2024-03-14 23:05:28 -0400

    updated quizzes/footer and fixed youtube iframe

--- bites-app/src/components/Footer.svelte
+++ bites-app/src/components/Footer.svelte
@@ -1,26 +1,26 @@
-<main>
-	<footer class="bg-base-300 border-t-2 border-primary">
-		<div class="flex justify-center p-4">
-			<ul class="column px-16">
-				<li class="font-bold">Site Links</li>
-				<li><a class="link link-hover" href="./home">Home</a></li>
-				<li><a class="link link-hover" href="./modules">Modules</a></li>
-				<li><a class="link link-hover" href="./quizzes">Quizzes</a></li>
-				<li><a class="link link-hover" href="./libraries">Libraries</a></li>
-			</ul>
-			<div class="column px-16">
-				<p class="font-bold">Related Links</p>
-				<p>
-					<a class="link link-hover" href="https://github.com/SWE-2024/COSC-4P02"
-						>GitHub Repository</a
-					>
-				</p>
-				<p><a class="link link-hover" href="https://brocku.ca/">Brock University</a></p>
-			</div>
-		</div>
+<main class="bottom-0 w-full">
+    <footer class="bg-base-300 border-t-2 border-primary">
+        <div class="flex justify-center p-4">
+            <ul class="column px-16">
+                <li class="font-bold">Site Links</li>
+                <li><a class="link link-hover" href="./home">Home</a></li>
+                <li><a class="link link-hover" href="./modules">Modules</a></li>
+                <li><a class="link link-hover" href="./quizzes">Quizzes</a></li>
+                <li><a class="link link-hover" href="./libraries">Libraries</a></li>
+            </ul>
+            <div class="column px-16">
+                <p class="font-bold">Related Links</p>
+                <p>
+                    <a class="link link-hover" href="https://github.com/SWE-2024/COSC-4P02"
+                        >GitHub Repository</a
+                    >
+                </p>
+                <p><a class="link link-hover" href="https://brocku.ca/">Brock University</a></p>
+            </div>
+        </div>
 
-		<div class="text-center bg-primary text-neutral-content p-4">
-			<p>© Brock Interactive Training for Engineering Students 2024. All Rights Reserved.</p>
-		</div>
-	</footer>
+        <div class="text-center bg-primary text-neutral-content p-4">
+            <p>© Brock Interactive Training for Engineering Students 2024. All Rights Reserved.</p>
+        </div>
+    </footer>
 </main>
