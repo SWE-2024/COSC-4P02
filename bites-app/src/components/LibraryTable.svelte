58e71bc5f24aed43200aa17040ea3a7d9b337896
0-20 <zerodashtwenty@protonmail.com>
2024-03-31 19:04:36 -0400

    Added tabs

---
+++ bites-app/src/components/LibraryTable.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    export let columns: any;
+    export let libraryTable: any;
+</script>
+
+<table class="table table-xl">
+    <thead>
+        <tr class="">
+            {#each columns as column}
+                <th class="text-center">{column}</th>
+            {/each}
+        </tr>
+    </thead>
+    <tbody>
+        {#each libraryTable as entry}
+            <tr class="hover:bg-base-200">
+                <!--
+        {#each row as cell}
+        <td contenteditable="false" bind:innerHTML={cell} />
+        {/each}
+        -->
+                <td contenteditable="false" class="w-[23%]" bind:innerHTML="{entry.title}"></td>
+                <td contenteditable="false" class="w-[38%]" bind:innerHTML="{entry.description}"></td>
+                <td>
+                    <a
+                        class="link link-hover w-[38%]"
+                        href="{entry.link}"
+                        target="”_blank”"
+                        rel="noopener noreferrer"
+                        contenteditable="false"
+                        bind:innerHTML="{entry.link}"
+                    ></a>
+                </td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
