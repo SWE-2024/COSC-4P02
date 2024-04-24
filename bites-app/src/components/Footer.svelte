e6e709d40cb69b825d5cdbac72bf30cf3a5ed875
julellisg <julianellisg@gmail.com>
2024-04-24 00:41:40 -0400

    Update Footer.svelte
    Minor adjustments

--- bites-app/src/components/Footer.svelte
+++ bites-app/src/components/Footer.svelte
@@ -3,7 +3,7 @@
 
 <main class="bottom-0 w-full">
 	<footer class="bg-base-300 border-t-2 border-primary">
-		<div class="flex justify-center p-4">
+		<div class="flex justify-center text-sm sm:text-base p-4">
 			<ul class="column px-16">
 				<li class="font-bold">Site Links</li>
 				<li><a class="link link-hover" href="./home">Home</a></li>
@@ -11,7 +11,7 @@
 				<li><a class="link link-hover" href="./quizzes">Quizzes</a></li>
 				<li><a class="link link-hover" href="./libraries">Libraries</a></li>
 			</ul>
-			<div class="column px-16">
+			<div class="column px-12 lg:px-16">
 				<p class="font-bold">Related Links</p>
 				<p>
 					<a class="link link-hover" href="https://github.com/SWE-2024/COSC-4P02"
@@ -22,7 +22,7 @@
 			</div>
 		</div>
 
-		<div class="text-center bg-primary text-neutral-content p-4">
+		<div class="text-xs sm:text-base text-center bg-primary text-neutral-content p-4">
 			<p>© Brock Interactive Training for Engineering Students 2024. All Rights Reserved.</p>
 		</div>
 	</footer>
